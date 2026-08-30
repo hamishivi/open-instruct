3d984a83412a77ff2936d88b93af9632bc3ecf3e
Hamish Ivison <hamishivi@gmail.com>
2026-08-30 06:26:59 -0700

    Use prepared Python for value snapshot scoring

--- scripts/eval/value_estimation/score_generative_value.sh
+++ scripts/eval/value_estimation/score_generative_value.sh
@@ -11,8 +11,9 @@ GEN_VALUE_MAX_NEW_TOKENS="${GEN_VALUE_MAX_NEW_TOKENS:-1024}"
 VLLM_TENSOR_PARALLEL_SIZE="${VLLM_TENSOR_PARALLEL_SIZE:-1}"
 VLLM_GPU_MEMORY_UTILIZATION="${VLLM_GPU_MEMORY_UTILIZATION:-0.85}"
 RUN_NAME="${RUN_NAME:-generative_value_${CONDITIONING}}"
+PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-python}
 
-uv run python -m open_instruct.value_estimation score_dataset \
+"${PYTHON_EXECUTABLE}" -m open_instruct.value_estimation score_dataset \
     --input_dataset_path "${INPUT_DATASET_PATH}" \
     --output_path "${OUTPUT_PATH}" \
     --value_model_path "${VALUE_MODEL_PATH}" \
